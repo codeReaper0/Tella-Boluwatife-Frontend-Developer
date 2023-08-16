db3ad055d4637dcf74c952040480300217354a2d
codeReaper0 <boluwatifetella@gmail.com>
2023-08-16 19:39:51 +0100

    added neta info

--- src/api/capsules.php
+++ src/api/capsules.php
@@ -1,13 +1,17 @@
 <?php
 header("Access-Control-Allow-Origin: http://localhost:3000");
-// header("Access-Control-Allow-Headers: Authorization, Content-Type");
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
+header("Access-Control-Allow-Methods: GET");
 
-// $authHeader = $_SERVER["HTTP_AUTHORIZATION"] ?? $_SERVER["REDIRECT_HTTP_AUTHORIZATION"];
-// $authToken = str_replace("Bearer ", "", $authHeader);
-// $token = "i_am_verified";
+$authHeader = $_SERVER["HTTP_AUTHORIZATION"] ?? null;
 
-// if ($authToken !== $token) {
+
+// Verify the token here, replace "i_am_verified" with your actual verification logic
+$token = "i_am_verified";
+
+// if ($authHeader !== "Bearer " . $token) {
 // 	http_response_code(401);
+// 	exit();
 // }
 
 $spacexApiUrl = "https://api.spacexdata.com/v3/capsules";
